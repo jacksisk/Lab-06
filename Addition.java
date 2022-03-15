78a5235195cc7b21e90cf2fc6c5ff16b4c0b5bc1
jacksisk <101613469+jacksisk@users.noreply.github.com>
2022-03-14 23:47:07 -0400

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class Addition {
+public class Addition{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         int selection;
         int op1;
         int op2;
         int sum;
-        System.out.println("Thank you for using this calculator. Select which operation you would like to make and enter your two operators.\n1. Addition\n Subtraction");
-        
+        System.out.println("Thank you for using this calculator. Select which operation you would like to make and enter your two operators.\n1. Addition\n2. Subtraction");
+        selection = input.nextInt();
         System.out.println("Enter the first integer");
         op1 = input.nextInt();
         System.out.println("Enter the second integer");
@@ -19,5 +19,6 @@ public class Addition {
             }else{
             sum = op1 - op2;
             System.out.print("The result is " + sum);
+            }
     }
 }
